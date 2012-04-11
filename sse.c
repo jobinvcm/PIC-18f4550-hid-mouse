2501395ec315a56d58e2ad0c2ee52786a96443ae
Jobin Mathew <jobinvcm@gmail.com>
2012-04-11 11:39:02 -0700

    Main file

---
+++ sse.c
@@ -0,0 +1,27 @@
+unsigned char readbuff[64] absolute 0x500;   // Buffers should be in USB RAM, please consult datasheet
+unsigned char writebuff[64] absolute 0x540;
+
+char cnt;
+char kk;
+
+void interrupt(){
+   USB_Interrupt_Proc();                   // USB servicing is done inside the interrupt
+}
+
+void main(void){
+  ADCON1 |= 0x0F;                         // Configure all ports with analog function as digital
+  CMCON  |= 7;                            // Disable comparators
+
+  HID_Enable(&readbuff,&writebuff);       // Enable HID communication
+
+  while(1){
+    while(!HID_Read())
+      ;
+
+    for(cnt=0;cnt<64;cnt++)
+      writebuff[cnt]=readbuff[cnt];
+
+    while(!HID_Write(&writebuff,64))
+      ;
+  }
+}
